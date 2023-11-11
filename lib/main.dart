2987f00f745f8ddd5fc856d073805ccf900900ec
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-10 19:22:59 -0500

    changed stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,98 @@ void main() => runApp(MaterialApp(
   home: Card(),
 ));
 
-class Card extends StatelessWidget {
-  const Card({super.key});
+class Card extends StatefulWidget {
+  //const Card({super.key});
+
+  @override
+  State<Card> createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  int ninjaLevel = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Card Example'),
+        title: const Text('Card Example'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
+            Center(
+              child: const CircleAvatar(
+                backgroundImage: AssetImage('assets/dummy_user.png'),
+                radius: 40.0,
+              ),
+            ),
+            const Divider(
+              height: 90.0,
+              color: Colors.grey,
+            ),
+            const Text(
               'Name',
               style: TextStyle(
                 color: Colors.grey,
-                letterSpacing: 2.0
+                letterSpacing: 2.0,
               ),
             ),
-          Text(
-            'Frank Sinatra',
-            style: TextStyle(
+            const SizedBox(height: 10.0),
+            const Text(
+              'Frank Sinatra',
+              style: TextStyle(
                 color: Colors.amberAccent,
-                letterSpacing: 2.0
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 30.0),
+            const Text(
+              'Current Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              '$ninjaLevel',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10.0),
+                Text(
+                  'frank@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
-
